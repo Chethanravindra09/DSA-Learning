5858dc18f91203ae7ff079893ec33b58066b505e
Chethanravindra09 <chethan.cravindra@gmail.com>
2024-01-07 21:44:51 +0530

    Quick Sort Algorithm Added

--- QuickSort.java
+++ QuickSort.java
@@ -1,27 +1,53 @@
 import java.util.Arrays;
 
+
+
 public class QuickSort {
+
+    public static int[] quickSort(int arr[], int low , int high){
+
+        if(low<high){
+            int pivotIndex=partition(arr, low, high);
+
+            quickSort(arr, low, pivotIndex-1);
+            quickSort(arr, pivotIndex+1, high);
+        }
+        return arr;  
+}
+
+public static int partition(int[] arr, int low, int high){
+
+    int pivot=arr[low];
+    int i=low;
+    int j=high;
+
+    while(i<j){
+        while(arr[i]<=pivot&&i<=high-1){
+            i++;
+        }
+
+        while(arr[j]>pivot&&j>=low+1){
+            j--;
+        }
+
+        if(i<j){
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+        }
+    }
+    int temp=arr[low];
+    arr[low]=arr[j];
+    arr[j]=temp;
+
+return j;
+}
+
 public static void main(String[] args) {
     int[] arr={4,6,2,5,7,9,1,3};
     int n=arr.length;
 
-    int j =n-1;
-    for(int i =1;i<arr.length;i++){
-        int pivot=arr[0];
-        if(arr[i]>pivot&&arr[j]<pivot){
-            int temp=arr[j];
-            arr[j]=arr[i];
-            arr[i]=temp;
-            j--;
-        }
-    }
-   int  i=n/2 -1;
-        int temp=arr[0];
-        arr[0]=arr[i];
-        arr[i]=temp;
-
-    
-
+    quickSort(arr, 0, n-1);
     System.out.println(Arrays.toString(arr));
 }
 
