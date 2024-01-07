586a266366f0bbc955948d59805c8ebdfb1e70c9
Chethanravindra09 <chethan.cravindra@gmail.com>
2024-01-07 17:48:07 +0530

    done

--- InsertionSort.java
+++ InsertionSort.java
@@ -23,7 +23,7 @@ class InsertionSort {
 
   // Driver code
   public static void main(String args[]) {
-    int[] data = { 9, 5, 1, 4, 3 };
+    int[] data = { 9, 5, 1, 4, 3 ,7};
     InsertionSort is = new InsertionSort();
     is.insertionSort(data);
     System.out.println("Sorted Array in Ascending Order: ");
