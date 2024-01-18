efe192272e93046d9b0e70412ab7466f17f9da80
Chethanravindra09 <chethan.cravindra@gmail.com>
2024-01-18 11:21:18 +0530

    binary to decimal in java

---
+++ BinarytoDecimal.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class BinarytoDecimal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Binary to convert into Decimal: ");
+        int n = sc.nextInt();
+        int num = n;
+        int Decimal=0;
+        int i=0;
+        while(n!=0){
+            int rem = n %10;
+            Decimal+=rem * Math.pow(2,i);
+            i++;
+            n=n/10;
+        }
+        System.out.println("Decimal number of given binary "+num+" is "+ Decimal);
+    }
+}
